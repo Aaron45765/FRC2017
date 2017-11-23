9c7cb20301782c56cbe6876ca599047ab91e9835
Aaron <aaron45765@gmail.com>
2017-11-22 22:20:34 -0800

    Updated Constants to reflect real robot

--- src/main/java/frc/team3863/robot/Constants.java
+++ src/main/java/frc/team3863/robot/Constants.java
@@ -4,10 +4,10 @@ package frc.team3863.robot;
  * Created by Aaron Fang on 11/5/2017.
  */
 public class Constants {
-    public static final int leftDriveA_ID = 0;
-    public static final int leftDriveB_ID = 0;
-    public static final int rightDriveA_ID = 0;
-    public static final int rightDriveB_ID = 0;
+    public static final int leftDriveA_ID = 2;
+    public static final int leftDriveB_ID = 12;
+    public static final int rightDriveA_ID = 3;
+    public static final int rightDriveB_ID = 15;
     public static final int rightDrive_Enc = 20;
     public static final int leftDrive_Enc = 20;
     public static final int WHEEL_SIZE = 6; //in
@@ -16,12 +16,12 @@ public class Constants {
     public static final int PARTNER_JOYSTICK_ID = 2;
     public static final int HIGH_GEAR_ID = 0;
     public static final int LOW_GEAR_ID = 1;
-    public static final int shooterA_ID = 0;
+    public static final int shooterA_ID = 1;
     public static final int shooterB_ID = 0;
-    public static final int feeder_ID = 0;
-    public static final int intake_ID = 0;
+    public static final int feeder_ID = 7;
+    public static final int intake_ID = 6;
     public static final int blender_ID = 0;
-    public static final int hood_ID = 0;
+    public static final int hood_ID = 5;
 
     public static final int GATE_OPEN_ID = 0;
     public static final int GATE_CLOSED_ID = 0;
